dd58ddc80a09108cad33bf776ab46a5ad0ef5071
Sourabh Shegane <sgsourabh30@gmail.com>
2020-09-23 19:30:36 +0530

    Created ShoppingDatabase interface to fire queries

---
+++ app/src/main/java/com/sourabhcodes/shopper/utilities/ShoppingDatabase.kt
@@ -0,0 +1,18 @@
+package com.sourabhcodes.shopper.utilities
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.sourabhcodes.shopper.model.ShoppingItem
+
+@Dao
+interface ShoppingDatabase {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item : ShoppingItem)
+
+    @Delete
+    suspend fun delete(item : ShoppingItem)
+
+    @Query("SELECT * from shopping_items")
+    suspend fun getShoppingItems():LiveData<List<ShoppingItem>>
+}
