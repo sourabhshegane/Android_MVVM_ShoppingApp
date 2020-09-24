4d0e0710425cc070af0f344fe6ed13a7987cfac5
Sourabh Shegane <sgsourabh30@gmail.com>
2020-09-24 17:05:27 +0530

    Created UI for RecyclerView Item Created RecyclerView Adapter

---
+++ app/src/main/java/com/sourabhcodes/shopper/other/ShoppingItemAdapter.kt
@@ -0,0 +1,50 @@
+package com.sourabhcodes.shopper.other
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sourabhcodes.shopper.R
+import com.sourabhcodes.shopper.data.db.entities.ShoppingItem
+import com.sourabhcodes.shopper.ui.shoppinglist.ShoppingViewModel
+import kotlinx.android.synthetic.main.shopping_item.view.*
+
+class ShoppingItemAdapter(
+    var items: List<ShoppingItem>,
+    private val viewModel: ShoppingViewModel
+) : RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+
+    inner class ShoppingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
+        return ShoppingViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val currentShoppingItem = items[position]
+        holder.itemView.tvName.text = currentShoppingItem.name
+        holder.itemView.tvAmount.text = "Rs. $currentShoppingItem"
+
+        holder.itemView.ivDelete.setOnClickListener {
+            viewModel.delete(currentShoppingItem)
+        }
+
+        holder.itemView.ivPlus.setOnClickListener {
+            currentShoppingItem.amount++
+            viewModel.upsert(currentShoppingItem)
+        }
+
+        holder.itemView.ivMinus.setOnClickListener {
+            if (currentShoppingItem.amount > 0) {
+                currentShoppingItem.amount--
+                viewModel.upsert(currentShoppingItem)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
